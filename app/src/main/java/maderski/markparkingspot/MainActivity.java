fc051ad6e94e412f3fa6ca2f456c96fe5a4a9658
Jason Maderski <maderski@gmail.com>
2016-08-27 12:05:54 -0400

    Removed unused activities

--- app/src/main/java/maderski/markparkingspot/MainActivity.java
+++ app/src/main/java/maderski/markparkingspot/MainActivity.java
@@ -12,7 +12,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         if(hasLocationPermission()) {
             Actions actions = new Actions(this);
             actions.checkIfCanGetLocation();
